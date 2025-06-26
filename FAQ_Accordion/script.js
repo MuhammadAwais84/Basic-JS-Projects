dac2c6cec6cd52834268c908b8aa07f84733d2e5
MuhammadAwais84 <muhammadawais07543@gmail.com>
2025-06-26 03:05:16 -0400

    Refactor FAQ Accordion styles and JavaScript functionality for improved interactivity and visual consistency

---
+++ FAQ_Accordion/script.js
@@ -0,0 +1,23 @@
+let btn=document.querySelectorAll('.question button');
+document.addEventListener("DOMContentLoaded", function() {
+    // Initially hide all answers
+    for (let answer of answers) {
+        answer.classList.add('hide');
+    }
+});
+
+for (btn of btn) {
+    btn.addEventListener("click",function () {
+        let answer = this.parentElement.nextElementSibling;
+        if (answer.classList.contains('hide')) {
+            answer.classList.remove('hide');
+            answer.classList.add('active');
+            this.textContent = 'x';
+        } else {
+            answer.classList.remove('active');
+            answer.classList.add('hide');
+            this.textContent = '+';
+        }
+        
+    })
+}
